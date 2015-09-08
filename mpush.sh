47f9c8efecd43550f8fbc436832ffcc4575ebe66
Steven Wells <stevewells20@gmail.com>
2015-09-08 15:35:44 -0700

    Added d3 to modules

--- mpush.sh
+++ mpush.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-set timeout 9999;
+#set timeout 9999;
 spawn git add --all .
 expect eof; 
 spawn git commit -m "Added d3 to modules" 
@@ -9,6 +9,7 @@ expect "'https://github.com': "
 #sleep 1
 send "stevewells20\n"
 expect "'https://stevewells20@github.com': "
+set timeout 9999;
 #sleep 2
 send "1qazXSW@\n\n"
 expect eof;
