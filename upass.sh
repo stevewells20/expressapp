246610437a44349445bc9bc748fd3a4375b56bba
Steven Wells <stevewells20@gmail.com>
2015-09-07 21:11:42 -0700

    Added d3 to modules

--- upass.sh
+++ upass.sh
@@ -6,10 +6,10 @@ spawn git commit -m "Added d3 to modules"
 expect eof;
 spawn git push origin testing
 expect "'https://github.com': "
-sleep 1
+#sleep 1
 send "stevewells20\n"
 expect "'https://stevewells20@github.com': "
-sleep 1
+#sleep 1
 send "1qazXSW@\n\n"
 expect eof;
 
