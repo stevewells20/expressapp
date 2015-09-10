ae87b144aa904c7ef3781ac5c8a5c5c1ce09e820
Steven Wells <stevewells20@gmail.com>
2015-09-09 20:35:20 -0700

    Added d3 to modules

--- public/stats.js
+++ public/stats.js
@@ -31,7 +31,7 @@ function averageDB(type){
 		// if (typeof(total) == "number") {total = result.total_rows;}
 		// else {throw "ERROR: total is NaN!";};
 		result.rows.forEach( function (entry) {
-        	result = entry.doc[type] + result;
+        	result = valueOf(entry.doc[type]) + result;
         	console.log('Result is:\t' + result)// + '\tType is:\t'typeof(result));
         	console.log(show(entry.doc[type])+": Entry.doc[type] type is : "+ typeof(entry.doc[type]));
      	})
