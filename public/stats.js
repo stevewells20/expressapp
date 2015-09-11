e0b05285d465aa49e074cfdf6684307db4715e96
Steven Wells <stevewells20@gmail.com>
2015-09-11 03:03:48 -0700

    Added d3 to modules

--- public/stats.js
+++ public/stats.js
@@ -51,7 +51,7 @@ localDB.sync(remoteDB, {
 		// return Percentage;
 
 		result.rows.forEach( function(el){
-		    el.doc.render('layout.jade', { title: 'Testing', el: el });
+		    el.doc.render('layout.jade', { title: 'Testing', test: el });
 		    console.log(el);
 
 	});
