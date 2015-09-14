313f5b2686041bbc60a1319d44978e56d1946ca6
Steven Wells <stevewells20@gmail.com>
2015-09-14 12:18:19 -0700

    Added d3 to modules

--- public/stats.js
+++ public/stats.js
@@ -43,7 +43,7 @@ localDB.sync(remoteDB, {
 		for (var el of Data.rows) {
 			//console.log('el.doc.percentage = '+el.doc.percentage);
 			Percolate.Percentage += el.doc.percentage;
-			console.log('Percolate.Percentage = '+Percolate.Percentage);
+			console.log('Percolate.Percentage = '+ Percolate.Percentage);
 		};
 		Percolate.Percentage = Percolate.Percentage / Data.total_rows;
 		// console.log('Percentage before div'+Percentage);
