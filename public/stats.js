fa2e3ddece94d01a1d8f94759f6f3c844ad3481a
Steven Wells <stevewells20@gmail.com>
2015-09-14 17:02:21 -0700

    Added d3 to modules

--- public/stats.js
+++ public/stats.js
@@ -41,7 +41,7 @@ localDB.sync(remoteDB, {
 		//var Y = new Array( Object.keys(Data.rows).length );
 		
 		//var X = Array.apply(null, Array( Object.keys(Data.rows).length )).map(function (_, i) {return i;});
-		var X = Array.apply(null, Array(100)).map(function (_, i) {return i;});
+		var X = Array.apply(null, Array(100)).map(function (_, i) {return i+1;});
 
 		console.log("!!! CURRENT: \n"+X);
 		for (var el of Data.rows) {
